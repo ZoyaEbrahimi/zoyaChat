20f3e0276d2505be35c419aae57c8bf71a6cabe7
ZoyaEbrahimi <zoyaebrahimi55@gmail.com>
2018-11-29 12:33:16 +0100

    First commit

---
+++ app.js
@@ -0,0 +1,54 @@
+// Require Library
+const express = require('express');
+const socketIO = require('socket.io');
+
+//create express server
+const app = express();
+
+// Serve static files in public folder
+app.use(express.static('public'));
+
+//Start server and listen on a port
+var server = app.listen(5001, () => {
+    console.log(`Server started on port 5001`);
+});
+
+//use server on port 5000 for SocketIO connections
+const io = socketIO(server);
+
+
+// What should socketIO do ?
+// Accept connection request from client and print user connected
+//Initialize empty array to store client connections
+var connections = [];
+
+io.on('connection', (socket) => {
+    connections.push(socket);
+    console.clear();
+    console.log(`${connections.length} user(s) connected`);
+
+    socket.on('disconnect', () => {
+        connections.splice(connections.indexOf(socket), 1);
+        console.clear();
+        console.log(`${connections.length} user(s) remaining`)
+    });
+    // get data from client and emit it to all clients
+    socket.on('chat', (data) => {
+        let username = data.username;
+        let message = data.message;
+
+        if (username == '' || message == '') {
+            socket.emit('espection', {
+                fielderr: errmsg
+            });
+        } else {
+            socket.emit('chat', data);
+            io.sockets.emit('chat', data);
+        }
+    });
+    // get user is typing from client and emit to all clients except you
+    socket.on('typing', (data) => {
+        socket.broadcast.emit('typing', data);
+    });
+
+});
---
+++ public/client.js
@@ -0,0 +1,35 @@
+// MAke connection to server
+var socket = io.connect('http://localhost:5001');
+
+//Query DOM
+var message = document.getElementById('message');
+username = document.getElementById('username');
+output = document.getElementById('output');
+btn = document.getElementById('send');
+feedback = document.getElementById('feedback');
+//Emit events on click of button
+btn.addEventListener('click', () => {
+
+
+    //Emit the data to be sent
+    socket.emit('chat', {
+        username: username.value,
+        message: message.value
+    });
+});
+
+//?Add "USER is Typing" on Keypress event
+
+message.addEventListener('keypress', () => {
+    socket.emit('typing', username.value);
+})
+
+//Display data in empty div on that event
+socket.on('chat', (data) => {
+    feedback.innerHTML = '';
+    output.innerHTML += '<p><strong>' + data.username + ': </strong>' + data.message + '</p>';
+});
+
+socket.on('typing', (data) => {
+    feedback.innerHTML = '<p>' + data + ' is typing a message...</p>';
+});
